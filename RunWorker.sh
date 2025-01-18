90c61df5028c85d46a18c4fe51c3e774587ffd95
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-01-17 23:35:54 -0500

    Adjusted Timing

--- RunWorker.sh
+++ RunWorker.sh
@@ -14,7 +14,7 @@ spawn ./config.sh
 # Provide the URL of the repository
 expect "What is the URL of your repository?" { send "$REPO\n" }
 
-sleep 1
+sleep 0.1
 
 # Provide the runner register token
 expect "What is your runner register token?" { send "$TOKEN\n" }
@@ -24,12 +24,12 @@ sleep 1
 # Provide default input for runner group
 expect -re "Enter the name of the runner group to add this runner to.*" { send "$RUNNERGROUP\n" }
 
-sleep 1
+sleep 0.1
 
 # Provide the runner name
 expect -re "Enter the name of runner:.*" { send "$RUNNERNAME\n" }
 
-sleep 1
+sleep 0.1
 
 # Accept the default labels
 expect -re "This runner will have the following labels:.*" { send "$RUNNERLABELS\n" }
@@ -39,7 +39,7 @@ sleep 1
 # Provide the work folder name
 expect -re "Enter name of work folder:.*" { send "$RUNNERWORKDIR\n" }
 
-sleep 1
+sleep 0.1
 
 expect eof
 
